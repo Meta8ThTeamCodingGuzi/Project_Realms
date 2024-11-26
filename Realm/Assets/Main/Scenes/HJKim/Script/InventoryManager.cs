285c6df261cd6ce9012d95740cf52726522dc0e9
hyeonji <h91789084@gmail.com>
2024-11-26 17:40:58 +0900

    일단 인벤 열닫까지 하는데
    스크립트는 원래 에셋이 가지고있는거랑 충돌해서 일단보류

--- Realm/Assets/Main/Scenes/HJKim/Script/InventoryManager.cs
+++ Realm/Assets/Main/Scenes/HJKim/Script/InventoryManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject inventoryUi;
+    public bool InventoryOpen;
+
+    public void Start()
     {
-        
+
+        inventoryUi.SetActive(false);
+        InventoryOpen = false; //일단 기본은 닫혀있기
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Update() //매 프레임마다 확인하는 이유는, 언제 인벤을 열지모르니까임 
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (InventoryOpen == false)
+            {
+                ShowInven();//인벤UI를 여는 동작을 담음
+            }
+            else
+            {
+                HideInven();//닫는 동작을 담음
+            }
+
+
+        }
+
     }
+
+
+    public void ShowInven()
+    {
+        inventoryUi.SetActive(true);
+        InventoryOpen = true;
+    }
+
+    public void HideInven()
+    {
+        inventoryUi.SetActive(false);
+        InventoryOpen = false;
+
+    }
+
+
 }
